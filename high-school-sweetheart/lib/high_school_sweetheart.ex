f16136f765e890998653d900bba5fbc4df0655b1
Aliaksiej Maroz <aliaksiejmaroz@hotmail.com>
2023-11-12 01:13:06 +0300

    Added "High School Sweetheart" task.

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,40 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name)
+    |> String.first()
+  end
+
+  def initial(name) do
+    first_letter(name)
+    |> String.upcase()
+    |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    [first, last] = String.split(full_name)
+
+    initial(first) <> " " <> initial(last)
+  end
+
+  def pair(full_name1, full_name2) do
+    fr = initials(full_name1)
+    sc = initials(full_name2)
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{fr}  +  #{sc}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
